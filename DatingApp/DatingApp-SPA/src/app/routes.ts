e3f8554ef38268a92c1abcaf6930176926822a15
Angel L. Castro <al.castro@gmail.com>
2018-09-25 13:31:54 +0200

    Prevent to reach several URLs when you're not logged in.

--- DatingApp/DatingApp-SPA/src/app/routes.ts
+++ DatingApp/DatingApp-SPA/src/app/routes.ts
@@ -6,10 +6,16 @@ import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-    { path: 'messages', component: MessagesComponent},
-    { path: 'lists', component: ListsComponent},
+    { path: '', component: HomeComponent},
+    { path: '', /*path='dummy' => localhost:4200/dummymembers */
+      runGuardsAndResolvers: 'always',
+      canActivate: [AuthGuard],
+      children: [
+        { path: 'members', component: MemberListComponent},
+        { path: 'messages', component: MessagesComponent},
+        { path: 'lists', component: ListsComponent}
+      ]
+    },
     // The order is important.
-    { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
