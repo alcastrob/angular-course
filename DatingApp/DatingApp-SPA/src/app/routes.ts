76f53050373a44233a9c9e574aa6fbbd47977efe
Angel Castro <al.castro@gmail.com>
2018-09-26 18:20:15 +0200

    Connected the profile button to the component, passing the id captured from the URL.

--- DatingApp/DatingApp-SPA/src/app/routes.ts
+++ DatingApp/DatingApp-SPA/src/app/routes.ts
@@ -4,14 +4,16 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
-    { path: '', /*path='dummy' => localhost:4200/dummymembers */
+    { path: '', /* path='dummy' => localhost:4200/dummymembers */
       runGuardsAndResolvers: 'always',
       canActivate: [AuthGuard],
       children: [
         { path: 'members', component: MemberListComponent},
+        { path: 'members/:id', component: MemberDetailComponent},
         { path: 'messages', component: MessagesComponent},
         { path: 'lists', component: ListsComponent}
       ]
