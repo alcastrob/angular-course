6e750ee39a820d5424c49a86e5ae7d0374cf9e37
Angel L. Castro <al.castro@gmail.com>
2018-09-27 01:06:13 +0200

    The button save can only be pressed if some changes are made.

--- DatingApp/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../../_models/user';
+import { AlertifyService } from '../../_services/alertify.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -8,9 +10,10 @@ import { User } from '../../_models/user';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   user: User;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -18,4 +21,9 @@ export class MemberEditComponent implements OnInit {
     });
   }
 
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile updated successfully');
+    this.editForm.reset(this.user); // You have to pass the user as parameter in order to not get blanks in the form
+  }
 }
