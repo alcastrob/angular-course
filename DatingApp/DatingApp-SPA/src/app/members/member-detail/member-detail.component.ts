034368440e32eac7a89b3d83723174e92f3bb976
Angel L. Castro <al.castro@gmail.com>
2018-09-26 21:32:56 +0200

    Added the member-detail content. Added global styling into styles.css.

--- DatingApp/DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp/DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -23,7 +23,7 @@ export class MemberDetailComponent implements OnInit {
     this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
       this.user = user;
     }, error => {
-      this.alertify.error(error)
+      this.alertify.error(error);
     });
   }
 }
