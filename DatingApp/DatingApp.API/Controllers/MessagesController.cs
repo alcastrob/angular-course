f1ca1d15b58bf4b1c1dbaca9722f7efa67c703fb
Angel L. Castro <al.castro@gmail.com>
2018-10-03 10:09:09 +0200

    Added the MessageForCreationDto. Added the CreateMessage action on the messages controller.

---
+++ DatingApp/DatingApp.API/Controllers/MessagesController.cs
@@ -0,0 +1,64 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Helpers;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [ServiceFilter(typeof(LogUserActivity))]
+    [Authorize]
+    [Route("api/users/{userId}/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IDatingRepository repository;
+        private readonly IMapper mapper;
+
+        public MessagesController(IDatingRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet("{id}", Name = "GetMessage")]
+        public async Task<IActionResult> GetMessage(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var messageFromRepo = await repository.GetMessage(id);
+            if (messageFromRepo == null)
+                return NotFound();
+
+            return Ok(messageFromRepo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreationDto)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            messageForCreationDto.SenderId = userId;
+
+            var recipient = await repository.GetUser(messageForCreationDto.RecipientId);
+            if (recipient == null)
+                return BadRequest("Could not find user");
+
+            var message = mapper.Map<Message>(messageForCreationDto);
+            repository.Add(message);
+
+            var messageToReturn = mapper.Map<MessageForCreationDto>(message);
+
+            if (await repository.SaveAll())
+                return CreatedAtRoute("GetMessage", new {id = message.Id}, messageToReturn);
+
+            throw new System.Exception("Creating the message failed on save");
+        }
+    }
+}
